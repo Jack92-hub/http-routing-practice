4685021f87daceafdf8901f62b1d1cac45c86213
Jack92-hub <jumrzokov@gmail.com>
2020-03-25 07:13:48 +0900

    added new project

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import Posts from '../../containers/Blog/Posts/Posts'
+import './Blog.css';
+import { Route, NavLink, Switch,Redirect } from 'react-router-dom'
+import NewPost from './NewPost/NewPost';
+
+
+class Blog extends Component {
+    render () {
+        return (
+            <div className = 'Blog'>
+                <header>
+                    <nav>
+                        <ul>
+                            <li ><NavLink to="/posts" exact > Posts </NavLink></li>
+                            <li ><NavLink to={{
+                                pathname:'/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}> New Post </NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/*<Route path="/" exact render = {() => <h1>Home</h1>} />
+                <Route path="/new-post"  render = {() => <h1>Home 2</h1>} />*/}
+                <Switch>
+                    <Route path ="/new-post" component={NewPost} />
+                    <Route path ="/posts" component={Posts} /> 
+                    <Redirect from='/' to='/posts'/>
+                    {/*<Route path ="/" component={Posts} /> */}
+                </Switch>
+            </div>
+        );
+    }
+}
+
+export default Blog;
